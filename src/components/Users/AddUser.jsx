5820cd7903aaf43b671b0bb3acd394f22b5e29d9
Muhammad Bilal <billu97.memon@gmail.com>
2021-05-03 00:28:37 +0500

    Button & Card Component Created

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+
+import classes from "./AddUser.module.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" placeholder="Username" />
-      <label htmlFor="age">Age</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" placeholder="Username" />
+        <label htmlFor="age">Age(Years)</label>
+        <input type="number" id="age" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 

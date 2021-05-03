1c8aa533404e1bdc38ca7c859933f678abe72461
Muhammad Bilal <billu97.memon@gmail.com>
2021-05-03 20:28:23 +0500

    Added Fragment Tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 
 function App() {
@@ -14,10 +14,10 @@ function App() {
   }
 
   return (
-    <div >
+    <Fragment>
       <AddUser onAddUser={AddUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
